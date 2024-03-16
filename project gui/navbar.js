a2d39a580f607eba2863d8ccf6d2036ec9d61436
Pranav Kolhe <143176974+TechWithPranav@users.noreply.github.com>
2024-03-16 14:51:28 +0530

    first gui commit

---
+++ project gui/navbar.js
@@ -0,0 +1,10 @@
+// navbar.js
+// navbar.js
+fetch('../navbar.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('navbar-placeholder').innerHTML = html;
+    })
+    .catch(error => {
+        console.error('Error fetching navbar:', error);
+    });
